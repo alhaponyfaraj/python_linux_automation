3b16b278db41bd2b500ac8eb1bcb75e047afac44
Faraj Alhapony <alhaponyfarag@gmail.com>
2020-12-01 23:23:26 +0200

    Create firewall_rules.sh

---
+++ firewall_rules.sh
@@ -0,0 +1,26 @@
+
+# firewall configuration script
+
+echo "Script Started: at "$(date)
+
+ufw_pkg='ufw'
+
+
+# Check if ufw is installed
+if [ $(dpkg-query -W -f='${Status}' $ufw_pkg 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+   sudo apt-get update && sudo apt-get install $ufw_pkg;
+else
+   echo "****** ufw is installed ******";
+fi
+
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
+
+# enable firewall service and reload
+sudo ufw enable
+sudo ufw reload
+
+echo "Script Finished: at "$(date)
+
