21b549b9f8d2995b8a4c1b431a202b3331fe178f
Faraj Alhapony <alhaponyfarag@gmail.com>
2020-12-02 18:27:19 +0200

    Update firewall_rules.sh
    fixes binary issue between dos and unix

--- firewall_rules.sh
+++ firewall_rules.sh
@@ -1,26 +1,18 @@
-
 # firewall configuration script
-
 echo "Script Started: at "$(date)
-
 ufw_pkg='ufw'
-
-
 # Check if ufw is installed
-if [ $(dpkg-query -W -f='${Status}' $ufw_pkg 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dpkg-query -W -f='${Status}' $ufw_pkg 2>/dev/null | grep -c "ok installe$
 then
-   sudo apt-get update && sudo apt-get install $ufw_pkg;
+   sudo yum install -y $ufw_pkg
 else
-   echo "****** ufw is installed ******";
+   echo "****** ufw is installed ******"
 fi
 
 sudo ufw allow ssh
 sudo ufw allow http
 sudo ufw allow https
-
 # enable firewall service and reload
 sudo ufw enable
 sudo ufw reload
-
 echo "Script Finished: at "$(date)
-
